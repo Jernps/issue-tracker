4d907aa704b667daa5c620d95efebe9c4fe0a777
Jernps <44694512+Jernps@users.noreply.github.com>
2023-10-22 09:32:10 +0700

    customize radix UI Theme

--- app/issue/page.tsx
+++ app/issue/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import {Button} from '@radix-ui/themes'
+import Link from 'next/link'
 
 const issuePage = () => {
   return (
     <div className='px-5'>
-      <Button> New Issue</Button>
-
+      <Button><Link href='/issue/new'>New Issue</Link></Button>
     </div>
   )
 }
