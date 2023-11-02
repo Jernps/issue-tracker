31c3ed8eb818c501d1d1289a703f7de05d3a2dc2
Jernps <44694512+Jernps@users.noreply.github.com>
2023-11-02 10:23:07 +0700

    Set up NextAuth

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,22 +1,23 @@
-import {  IssueStatusBadge, Link } from '@/app/components';
+import { IssueStatusBadge, Link } from '@/app/components';
 import prisma from '@/prisma/client';
 import { Table } from '@radix-ui/themes';
-import delay from 'delay';
 import IssueActions from './IssueActions';
 
 
 const issuePage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(2000);
+
 
   return (
     <div>
       <IssueActions />
       <Table.Root variant='surface'>
       <Table.Header>
-        <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-        <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-        <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
+        <Table.Row>
+          <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
+        </Table.Row>
       </Table.Header>
       <Table.Body>
         {issues.map( issue => (
