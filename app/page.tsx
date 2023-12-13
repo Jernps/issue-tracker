694e1616b56a6a096512fd9600e5c1998c311e1f
Jernps <44694512+Jernps@users.noreply.github.com>
2023-12-13 15:38:46 +0700

    build pagination layout component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-
-import Image from 'next/image'
+import Image from "next/image";
+import Pagination from "./components/pagination";
 
 export default function Home() {
   return (
-    <div>Hello world</div>
-  )
+    <>
+      <div>Hello world</div>
+      <Pagination itemCount={100} pageSize={10} currentPage={10} />
+    </>
+  );
 }
